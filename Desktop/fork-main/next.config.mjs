a3b7d5d225a52f2cca45857ba85ca0eed6c2f6b4
Sutan Tadese <93734061+sutanmuleta@users.noreply.github.com>
2025-06-01 00:05:14 -0400

    initial deploy setup

--- Desktop/fork-main/next.config.mjs
+++ Desktop/fork-main/next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/sutan',
+    basePath: '/sutansite',
+    assetPrefix: '/sutansite/',
     images: {
       unoptimized: true,
     },
