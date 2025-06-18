f2fdba3e275ed9e9f2cb9d9bbb660f971b0f5cbd
Sutan Tadese <93734061+sutanmuleta@users.noreply.github.com>
2025-06-17 22:42:44 -0400

    Update tailwind.config.ts for production build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,11 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/**/*.{js,ts,jsx,tsx,mdx}" // ✅ This wildcard ensures no files are missed
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/constants/**/*.{js,ts,jsx,tsx,mdx}",
+    "./index.tsx"
   ],
   theme: {
     extend: {
